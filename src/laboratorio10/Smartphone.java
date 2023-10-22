c315bcd4ef8f46e743daa7a877b736bbc5d69f65
AnghyVelasquezRojas04 <142699682+AnghyVelasquezRojas04@users.noreply.github.com>
2023-10-22 14:44:28 -0500

    Add files via upload

---
+++ src/laboratorio10/Smartphone.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio10;
+
+/**
+ *
+ * @author anghy
+ */
+public class Smartphone {
+    
+    private String marca;
+    private String modelo;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+}
